ff87c721a93ea3161fea4916e41e013dd9e78585
Emil Åkerman <emilaakerman@gmail.com>
2025-08-06 13:26:48 +0200

    change from FlutterActivity to FlutterFragmentActivity in MainActivity.kt
    also add this import import io.flutter.embedding.android.FlutterFragmentActivity

--- android/app/src/main/kotlin/com/example/stripe_example_setup/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stripe_example_setup/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.stripe_example_setup
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
