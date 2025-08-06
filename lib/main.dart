27e6ecd4c03da6f1f9a266a76642e923e5b9e521
Emil Åkerman <emilaakerman@gmail.com>
2025-08-06 14:20:38 +0200

    create basic test ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:stripe_example_setup/stripe_constants.dart';
+import 'package:stripe_example_setup/stripe_service.dart';
 
-void main() {
+void main() async {
+  await _setup();
   runApp(const MainApp());
+}
+
+Future<void> _setup() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = StripeConstants.stripePublishableKey;
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +18,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Flutter Stripe Example Setup'),
+              ElevatedButton(
+                onPressed: () {
+                  StripeService.I.makePayment();
+                },
+                child: Text("Press"),
+              ),
+            ],
+          ),
         ),
       ),
     );
