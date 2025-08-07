5631344878ef99466b2423b873926a073cc41071
Emil Åkerman <emilaakerman@gmail.com>
2025-08-07 13:53:39 +0200

    Update stripe_constants.dart

--- lib/stripe_constants.dart
+++ lib/stripe_constants.dart
@@ -1,6 +1,4 @@
 /// Contains the constant variables for connecting to the Stripe API.
-/// Obviously do not push the secret key to github.
 class StripeConstants {
   static const String stripePublishableKey = "";
-  static const String stripeSecretKey = "";
 }
